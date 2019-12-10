cc08394060dc55691a6e21120670477442a90cad
Aniko Borosova <borosanikoo@gmail.com>
2019-12-10 19:16:25 +0100

    started

--- hazi.js
+++ hazi.js
@@ -1,3 +1,11 @@
 const fs = require("fs");
 
-fs.writeFileSync("./myfile.html", "test", "utf8");
+const tableContent = "<table>";
+
+for (var i = 0; i < 10; i++) {
+
+}
+
+tableContent += "</table>";
+
+fs.writeFileSync("./myfile.html", tableContent, "utf8");
