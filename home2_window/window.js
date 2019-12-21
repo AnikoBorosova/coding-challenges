8f72641e0bbf52cf16bc284e29e213a485a7ee8c
Anikó Borosová <borosanikoo@gmail.com>
2019-12-21 16:33:06 +0100

    diamon & window

--- home2_window/window.js
+++ home2_window/window.js
@@ -1,26 +1,26 @@
 const fs = require("fs");
 
 let resultString = "<table cellpadding='1' cellspacing='1'>\n";
-let diag = 6;
+let diag = 14;
 
-for (let i = 0; i < diag; i++) {
+for (let i = 1; i <= diag; i++) {
 	resultString += "<tr>\n";
 
-	for (let j = 0; j < diag; j++) {
+	for (let j = 1; j <= diag; j++) {
 
-		if ((i < diag / 2) && (j < diag / 2) && (i + j == diag / 2 - 1)) {
-
-			resultString += "<td width='20' height='20' style='background: rgb(0, 0, 0)'></td>\n";
-
-		} else if ((i < diag / 2) && (j >= diag / 2) && (i + diag / 2 == j) ) {
+		if ((i == diag / diag) || (j == diag / diag)) {
 
 			resultString += "<td width='20' height='20' style='background: rgb(0, 0, 0)'></td>\n";
 
-		} else if ((i >= diag / 2) && (j < diag / 2) && (i - diag / 2 == j)) {
+		} else if ((i == diag / 2) || (j == diag / 2)) {
 
 			resultString += "<td width='20' height='20' style='background: rgb(0, 0, 0)'></td>\n";
 
-		} else if ((i >= diag / 2) && (j >= diag / 2) && (i + j == diag - 1 + diag / 2)) {
+		} else if ((i == diag / 2 + 1) || (j == diag / 2 + 1)) {
+
+			resultString += "<td width='20' height='20' style='background: rgb(0, 0, 0)'></td>\n";
+
+		} else if ((i == diag) || (j == diag)) {
 
 			resultString += "<td width='20' height='20' style='background: rgb(0, 0, 0)'></td>\n";
 
