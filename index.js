24caaa602d5efc0ec903fc43bcedd193448232d6
Anikó Borosová <borosanikoo@gmail.com>
2020-01-10 15:44:27 +0100

    started

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const fs = require("fs");
 const tree = require("./tree.json");
+const body = tree.body;
 
 let resultString = "<body>\n";
+
+for (let i = 0; i <= body.children.length; i++) {
+
+	resultString += ``
+
+}
+
+
+
+
 
 //if (tree.body.children.type === "text") {
 	resultString += `${tree.body.children[0].value}`;
