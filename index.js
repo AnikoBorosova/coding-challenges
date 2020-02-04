45a47f5d0cf03c9ca156ccec71fbf2630b520717
Anikó Borosová <borosanikoo@gmail.com>
2020-02-04 12:45:43 +0100

    ..

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 const fs = require("fs");
 const tree = require("./tree.json");
-const body = tree.body;
 
-let resultString = "<body>\n";
 
+//let resultString = "<body>\n";
+
+
+/*
 for (let i = 0; i <= body.children.length; i++) {
 
 	resultString += ``
 
+}*/
+
+//
+
+function writeType(dog) {
+ 
+ 	console.log(dog.type);
+
+ 	for (let i = 0; i < dog.children.length; i++) {
+ 		const elem = dog.children[i];
+
+ 		
+
+ 		console.log(elem.type);
+ 		
+ 		writeType(elem.children[i]);
+ 	}
 }
 
+
+writeType(tree.body);
 
 
 
 
 //if (tree.body.children.type === "text") {
-	resultString += `${tree.body.children[0].value}`;
+	//resultString += `${tree.body.children[0].value}`;
 //}
 
 // for loop a childrenen
@@ -24,11 +45,11 @@ for (let i = 0; i <= body.children.length; i++) {
 //a fuggveny onmagat hivogatja
 //de kell egy legelso hivas a body-ra
 
-resultString = "</body>";
+//resultString = "</body>";
 
 
 
 
 
 
-fs.writeFileSync("./temp.html", resultString, "utf8");
+//fs.writeFileSync("./temp.html", resultString, "utf8");
