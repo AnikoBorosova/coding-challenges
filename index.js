08e72a718170e4291dfc47cee9e69dbe231ed35e
Anikó Borosová <borosanikoo@gmail.com>
2020-02-21 15:19:12 +0100

    fixi

--- index.js
+++ index.js
@@ -1,50 +1,88 @@
 const fs = require("fs");
+
 const tree = JSON.parse(fs.readFileSync("tree.json", "utf8"));
+
+console.log(tree);
 
 const textPattern = `"%%VALUE%%"`;
 const imagePattern = `image: (%%SRC%%)`;
 
-function generateHTMLForNode(type, childrenContent, node) {
+function generatePlainTextForNode(type, childrenContent, node) {
     if (type === "text") {
-        return `<div class="nodetext">${node.value}</div>"`;
+        return `"${node.value}"`;
     }
-
     if (type === "image") {
-        return `<div class="nodeimage"><img src="${node.src}" alt="${node.alt || ""}" /></div>`;
+        return `image: (${node.src})`;
     }
-
     if (type === "body") {
-        return `<!DOCTYPE HTML><html> <head></head> <body style="background-color: ${node.background}">
-<div class="vertical">${childrenContent.join("<div>\n</div class=\"vertical\">")}</div>
-</body></html>`;
+        return `==========================================
+${childrenContent.join("\n------------------------------------------\n")}
+==========================================`
     }
-
     if (type === "col") {
-        return `<div class="vertical">${childrenContent.join("<div>\n</div class=\"vertical\">")}</div>`;
+        return childrenContent.join("\n------------------------------------------\n");
     }
-
     if (type === "multicol") {
-        return `<div class="horizontal">${childrenContent.join("<div>\n</div class=\"horizontal\">")}</div>`;
+        return childrenContent.join("\n++++++++++++++++++++++++++++++++++++++++++\n");
     }
-
     return `type: ${type} \n ${childrenContent.join("\n")}`;
 }
 
-
-function getHtml(node) {
-
+function plainText(node) {
     const type = node.type;
     const childrenContent = [];
+    let i = 0;
     if (node.children) {
         node.children.forEach(child => {
-            childrenContent.push(getHtml(child));
+            // childrenContent.push(plainText(child)) 
+            childrenContent[i] = plainText(child);
+            i++;
+        });
+    }
+    return generatePlainTextForNode(type, childrenContent, node);
+}
+
+function generateHTMLForNode(type, childrenContent, node) {
+    if (type === "text") {
+        return `<div class="nodetext">${node.value}</div>`;
+    }
+    if (type === "image") {
+        return `<div class="nodeimage"><img src="${node.src}" alt="${node.alt || ""}" /></div>`;
+    }
+    if (type === "body") {
+        return `<!DOCTYPE HTML><html> <head></head> <body style="background-color: ${node.background}">
+<div class="vertical">${childrenContent.join("</div>\n<div class=\"vertical\">")}</div>
+</body></html>`;
+    }
+    if (type === "col") {
+        return `
+<div class="vertical">${childrenContent.join("</div>\n<div class=\"vertical\">")}</div>
+`;
+    }
+    if (type === "multicol") {
+        return `
+<div class="horizontal">${childrenContent.join("</div>\n<div class=\"horizontal\">")}</div>
+`;
+    }
+    return `type: ${type} \n ${childrenContent.join("\n")}`;
+}
+
+function getHTML(node) {
+    const type = node.type;
+    const childrenContent = [];
+    let i = 0;
+    if (node.children) {
+        node.children.forEach(child => {
+            // childrenContent.push(plainText(child)) 
+            childrenContent[i] = getHTML(child);
+            i++;
         });
     }
     return generateHTMLForNode(type, childrenContent, node);
-    //return type + "\n" + childrenContent.join("\n");
-
 }
 
-console.log(getHtml(tree.body));
+console.log(getHTML(tree.body));
 
-fs.writeFileSync("./generatedHTML.html", getHtml(tree.body));
+fs.writeFileSync("./generatedHTML.html", getHTML(tree.body));
+
+
